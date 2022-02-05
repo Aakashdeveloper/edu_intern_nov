57ff68aa38dc5085f8380c898dd2c90e1ea46716
aakashdeveloper <ahanda205@gmail.com>
2022-02-05 17:41:31 +0530

    api1,2,3

--- node/restapi/Apidocument.md
+++ node/restapi/Apidocument.md
@@ -1,29 +1,59 @@
 Page 1
-List of city    
+List of city
+> http://localhost:6700/location
+> https://zomatoajulypi.herokuapp.com/location
 List of restaurants 
-hotels wrt to city 
-quick search data   
+> http://localhost:6700/restaurants
+> https://zomatoajulypi.herokuapp.com/restaurants
+restaurants wrt to city 
+> http://localhost:6700/restaurants?state_id=4
+> https://zomatoajulypi.herokuapp.com/restaurant?stateId=1
+quick search data  
+> http://localhost:6700/mealType
+> https://zomatoajulypi.herokuapp.com/quicksearch
 
+(http://localhost:6700/restaurants?state_id=4&meal_id=5)
 Page 2
 restaurants wrt to quickSearch 
+> http://localhost:6700/restaurants?meal_id=5
+> https://zomatoajulypi.herokuapp.com/restaurant?mealtype_id=
+
 
 filter
 > cuisine filter
   data respect to cuisine and quickSearch 
+  > http://localhost:6700/filter/3?cuisine=4
+  > https://zomatoajulypi.herokuapp.com/filter/4?cuisine=1
 > cost filter
+  > http://localhost:6700/filter/1?lcost=200&hcost=500
+  > https://zomatoajulypi.herokuapp.com/filter/1?hcost=1000&lcost=500
  data respect to cuisine and cost 
 > cuisine filter + cost filter 
+  > http://localhost:6700/filter/1?lcost=200&hcost=500&cuisineId=1
+
 > sort
     sort low to high in same quickSearch
+    http://localhost:6700/filter/1?cuisineId=1&sort=1
     sort high to low in same quickSearch
+    http://localhost:6700/filter/1?cuisineId=1&sort=-1
+
 > pagination
+   > http://localhost:6700/filter/1?cuisineId=1&skip=2&limit=2
 
 
 Page 3
 > restaurants details
+> http://localhost:6700/details/2
+> http://zomatoajulypi.herokuapp.com/details/1
+
 > Menu of that restaurants
+> http://localhost:6700/menu/2
+> https://zomatoajulypi.herokuapp.com/menu/1
+
 
 page 4
+> menu items on user selection
+
 > api to place order
 
 page 5
@@ -32,19 +62,6 @@ page 5
  
 
 /////
- (get) restaurants>         https://zomatoajulypi.herokuapp.com/restaurants
-(get) restwrtcity >        https://zomatoajulypi.herokuapp.com/restaurant?stateId=1
-(get) hoteldetails >       http://zomatoajulypi.herokuapp.com/details/1
-(get) quickSearch>         https://zomatoajulypi.herokuapp.com/quicksearch
-(get) onbasisofMeal >      https://zomatoajulypi.herokuapp.com/restaurant?mealtype_id=
-
-(get) cuisinefilter >      https://zomatoajulypi.herokuapp.com/filter/4?cuisine=1
-
-(get) costfilter >         https://zomatoajulypi.herokuapp.com/filter/1?hcost=1000&lcost=500
-
-(get) cityname >           https://zomatoajulypi.herokuapp.com/location
-(get) menu >               https://zomatoajulypi.herokuapp.com/menu/1
-
 (post) orderspage >        http://zomatoajulypi.herokuapp.com/menuItem
                            body [1,4,5]
 (get) allorder>            
--- node/restapi/app.js
+++ node/restapi/app.js
@@ -22,6 +22,100 @@ app.get('/location',(req,res) => {
     })
 })
 
+//restaurants as per location
+/*app.get('/restaurants/:id',(req,res) => {
+    let restId  = Number(req.params.id)
+    console.log(">>>>restId",restId)
+    db.collection('restaurants').find({state_id:restId}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})*/
+app.get('/restaurants',(req,res) => {
+    let stateId  = Number(req.query.state_id)
+    let mealId = Number(req.query.meal_id)
+    let query = {};
+    if(stateId && mealId){
+        query = {"mealTypes.mealtype_id":mealId,state_id:stateId}
+    }
+    else if(stateId){
+        query = {state_id:stateId}
+    }
+    else if(mealId){
+        query = {"mealTypes.mealtype_id":mealId}
+    }
+    console.log(">>>>restId",stateId)
+    db.collection('restaurants').find(query).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// filters
+app.get('/filter/:mealId',(req,res) => {
+    let sort = {cost:1}
+    let mealId = Number(req.params.mealId)
+    let skip = 0;
+    let limit = 100000000000000;
+    let cuisineId =  Number(req.query.cuisine)
+    let lcost = Number(req.query.lcost);
+    let hcost = Number(req.query.hcost);
+    let query = {}
+    if(req.query.sort){
+        sort = {cost:req.query.sort}
+    }
+    if(req.query.skip && req.query.limit){
+        skip = Number(req.query.skip);
+        limit = Number(req.query.limit);
+    }
+    if(cuisineId&lcost&hcost){
+        query = {
+            "cuisines.cuisine_id":cuisineId,
+            "mealTypes.mealtype_id":mealId,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}]
+        }
+    }
+    else if(cuisineId){
+        query = {"cuisines.cuisine_id":cuisineId,"mealTypes.mealtype_id":mealId}
+    }
+    else if(lcost&hcost){
+        query = {$and:[{cost:{$gt:lcost,$lt:hcost}}],"mealTypes.mealtype_id":mealId}
+    }
+
+    db.collection('restaurants').find(query).sort(sort).skip(skip).limit(limit).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// mealType
+app.get('/mealType',(req,res) => {
+    db.collection('mealType').find().toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//rest details
+app.get('/details/:id',(req,res) => {
+    let restId  = Number(req.params.id)
+    // let restId = mongo.ObjectId(req.params.id)
+    db.collection('restaurants').find({restaurant_id:restId}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//menu wrt to rest
+app.get('/menu/:id',(req,res) => {
+    let restId  = Number(req.params.id)
+    db.collection('menu').find({restaurant_id:restId}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// menu on basis of user selection>> Todo
 
 MongoClient.connect(mongoUrl, (err,client) => {
     if(err) console.log("Error While Connecting");
